61417ab3c4827280644ed0be8002eeab92cd5391
LilyStilson <lunamusicoff@gmail.com>
2025-03-21 00:03:19 +0100

    fix: moved SkillTag enum to separate file; fix: added AttributeAnomaly to skill tags

--- InterknotCalculator/Classes/Skill.cs
+++ InterknotCalculator/Classes/Skill.cs
@@ -1,15 +1,8 @@
-﻿using InterknotCalculator.Classes.Agents;
-using InterknotCalculator.Enums;
+﻿using InterknotCalculator.Enums;
 
 namespace InterknotCalculator.Classes;
 
 public record Scale(double Damage, double Daze, Element? Element = null);
-
-public enum SkillTag {
-    BasicAtk, Dash, Counter, 
-    QuickAssist, DefensiveAssist, EvasiveAssist, FollowUpAssist,
-    Special, ExSpecial, Chain, Ultimate
-}
 
 public record Skill {
     public SkillTag Tag { get; set; }
---
+++ InterknotCalculator/Enums/SkillTag.cs
@@ -0,0 +1,9 @@
+namespace InterknotCalculator.Enums;
+
+public enum SkillTag {
+    BasicAtk, Dash, Counter, 
+    QuickAssist, DefensiveAssist, EvasiveAssist, FollowUpAssist,
+    Special, ExSpecial, Chain, Ultimate,
+    
+    AttributeAnomaly
+}
