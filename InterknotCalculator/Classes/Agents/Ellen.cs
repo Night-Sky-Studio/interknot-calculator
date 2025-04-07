e72f039a346459565f94b55a2d2388ac19c6fa5d
LilyStilson <lunamusicoff@gmail.com>
2025-04-07 19:54:00 +0200

    feat: added agent 1191 and their signature weapon 14119

---
+++ InterknotCalculator/Classes/Agents/Ellen.cs
@@ -0,0 +1,126 @@
+﻿using InterknotCalculator.Enums;
+
+namespace InterknotCalculator.Classes.Agents;
+
+public class Ellen : Agent {
+    public Ellen() {
+        Speciality = Speciality.Attack;
+        Element = Element.Ice;
+        Rarity = Rarity.S;
+
+        Stats[Affix.Hp] = 7673;
+        Stats[Affix.Def] = 606;
+        Stats[Affix.Atk] = 863;
+        Stats[Affix.CritRate] = 0.05 + 0.144;
+        Stats[Affix.CritDamage] = 0.5;
+        Stats[Affix.Impact] = 93;
+        Stats[Affix.AnomalyMastery] = 94;
+        Stats[Affix.AnomalyProficiency] = 93;
+
+        Skills["saw_teeth_trimming"] = new() {
+            Tag = SkillTag.BasicAtk,
+            Scales = [
+                new(98.3, 37.6, Element.Physical),
+                new(222.2, 130.8, Element.Physical),
+                new(478.6, 306.8, Element.Physical),
+            ]
+        };
+        Skills["flash_freeze_trimming"] = new() {
+            Tag = SkillTag.BasicAtk,
+            Scales = [
+                new(199.7, 74.1),
+                new(368.8, 135.3),
+                new(993.4, 368.7),
+            ]
+        };
+
+        Skills["arctic_ambush"] = new() {
+            Tag = SkillTag.Dash,
+            Scales = [
+                new(125, 94.2),
+                new(266.8, 152.2),
+                new(316.6, 188.9)
+            ]
+        };
+        Skills["monstrous_wave"] = new() {
+            Tag = SkillTag.Dash,
+            Scales = [
+                new(154, 58.3, Element.Physical)
+            ]
+        };
+        Skills["cold_snap"] = new() {
+            Tag = SkillTag.Dash,
+            Scales = [
+                new(292, 118.4)
+            ]
+        };
+        Skills["reef_rock"] = new() {
+            Tag = SkillTag.Counter,
+            Scales = [
+                new(305.5, 341.8)
+            ]
+        };
+
+        Skills["shark_sentinel"] = new() {
+            Tag = SkillTag.QuickAssist,
+            Scales = [
+                new(243.2, 182.7)
+            ]
+        };
+        Skills["wavefront_impact"] = new() {
+            Tag = SkillTag.DefensiveAssist,
+            Scales = [
+                new (0, 407.7),
+                new (0, 530),
+                new (0, 250.4),
+            ]
+        };
+        Skills["shark_cruiser"] = new() {
+            Tag = SkillTag.FollowUpAssist,
+            Scales = [
+                new(876.8, 577.3)
+            ]
+        };
+
+        Skills["drift"] = new() {
+            Tag = SkillTag.Special,
+            Scales = [
+                new(101.1, 75.8)
+            ]
+        };
+        Skills["tail_swipe"] = new() {
+            Tag = SkillTag.ExSpecial,
+            Scales = [
+                new(754.5, 608.6)
+            ]
+        };
+        Skills["sharknami"] = new() {
+            Tag = SkillTag.ExSpecial,
+            Scales = [
+                new(1106.6, 557.6)
+            ]
+        };
+
+        Skills["avalanche"] = new() {
+            Tag = SkillTag.Chain,
+            Scales = [
+                new(1589.9, 533.9)
+            ]
+        };
+        Skills["endless_winter"] = new() {
+            Tag = SkillTag.Ultimate,
+            Scales = [
+                new(3781.7, 278.7)
+            ]
+        };
+    }
+
+    public override Stat? ApplyAbilityPassive(string ability) {
+        switch (ability) {
+            case "arctic_ambush" or "flash_freeze_trimming":
+                return new(1, Affix.CritDamage);
+            default:
+                return null;
+        }
+    }
+}
--- InterknotCalculator/Classes/Calculator.cs
+++ InterknotCalculator/Classes/Calculator.cs
@@ -39,6 +39,7 @@ public class Calculator {
     private static Agent CreateAgentInstance(uint agentId) {
         return agentId switch {
             1091 => new Miyabi(),
+            1191 => new Ellen(),
             1241 => new ZhuYuan(),
             1261 => new JaneDoe(),
             _ => throw new ArgumentOutOfRangeException(nameof(agentId), agentId, "Agent wasn't found.")
