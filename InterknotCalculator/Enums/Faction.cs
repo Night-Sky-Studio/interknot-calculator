e8c1dfdd682ea3b00ace1fdb594f84c84468aac1
LilyStilson <lunamusicoff@gmail.com>
2025-04-18 22:09:59 +0200

    fix: fixed incorrect values of flags enum Faction

--- InterknotCalculator/Enums/Faction.cs
+++ InterknotCalculator/Enums/Faction.cs
@@ -2,16 +2,16 @@
 
 [Flags]
 public enum Faction {
-    BelobogHeavyIndustries,
-    CriminalInvestigationResponseTeam,
-    CunningHares,
-    SilverSquad,
-    HollowSpecialOperationsSection6,
-    LyreSquad,
-    Mockingbird,
-    ObolSquad,
-    SonsOfCalydon,
-    StarsOfLyra,
-    VictoriaHousekeeping,
+    BelobogHeavyIndustries = 0,
+    CriminalInvestigationResponseTeam = 1,
+    CunningHares = 2,
+    SilverSquad = 4,
+    HollowSpecialOperationsSection6 = 8,
+    LyreSquad = 16,
+    Mockingbird = 32,
+    ObolSquad = 64,
+    SonsOfCalydon = 128,
+    StarsOfLyra = 256,
+    VictoriaHousekeeping = 512,
     NewEriduDefenseForce = SilverSquad | ObolSquad | LyreSquad
 }
