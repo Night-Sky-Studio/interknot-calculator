a700d490d1068e0614508d7141d2839172af9a50
LilyStilson <lunamusicoff@gmail.com>
2025-04-19 22:30:34 +0200

    feat: separated agent's stats into base and bonus with accessor for each stat

--- InterknotCalculator/Classes/Agents/Agent.cs
+++ InterknotCalculator/Classes/Agents/Agent.cs
@@ -12,10 +12,50 @@ public abstract class Agent {
     public Rarity Rarity { get; set; }
     public Faction Faction { get; set; }
     public SafeDictionary<Affix, double> Stats { get; set; } = new();
+    public SafeDictionary<Affix, double> BonusStats { get; set; } = new();
     public SafeDictionary<Affix, double> ExternalBonus { get; set; } = new();
     public SafeDictionary<SkillTag, Stat> ExternalTagBonus { get; set; } = new();
     public Dictionary<string, Anomaly> Anomalies { get; set; } = new();
     public Dictionary<string, Skill> Skills { get; set; } = new();
+    
+    public Affix RelatedElementDmg => Helpers.GetRelatedAffixDmg(Element);
+    public Affix RelatedElementRes => Helpers.GetRelatedAffixRes(Element);
+    
+    public double Hp => Stats[Affix.Hp] * (1 + BonusStats[Affix.HpRatio]) + BonusStats[Affix.Hp];
+    public double Atk => Stats[Affix.Atk] * (1 + BonusStats[Affix.AtkRatio]) + BonusStats[Affix.Atk];
+    public double Def => Stats[Affix.Def] * (1 + BonusStats[Affix.DefRatio]) + BonusStats[Affix.Def];
+    public double Pen => Stats[Affix.Pen] + BonusStats[Affix.Pen];
+    public double PenRatio => Stats[Affix.PenRatio] + BonusStats[Affix.PenRatio];
+    public double CritRate => Math.Min(Stats[Affix.CritRate] + BonusStats[Affix.CritRate], 1);
+    public double CritDamage => Stats[Affix.CritDamage] + BonusStats[Affix.CritDamage];
+    public double Impact => Stats[Affix.Impact] * (1 + BonusStats[Affix.ImpactRatio]) + BonusStats[Affix.Impact];
+    public double AnomalyMastery => Stats[Affix.AnomalyMastery] * (1 + BonusStats[Affix.AnomalyMasteryRatio]) + BonusStats[Affix.AnomalyMastery];
+    public double AnomalyProficiency => Stats[Affix.AnomalyProficiency] + BonusStats[Affix.AnomalyProficiency];
+    public double EnergyRegen => Stats[Affix.EnergyRegen] * (1 + BonusStats[Affix.EnergyRegenRatio]) + BonusStats[Affix.EnergyRegen];
+    public double ElementalDmgBonus => Stats[RelatedElementDmg] + BonusStats[RelatedElementDmg];
+    public double ElementalResPen => Stats[RelatedElementRes] + BonusStats[RelatedElementRes];
+    public double DmgBonus => Stats[Affix.DmgBonus] + BonusStats[Affix.DmgBonus];
+    public double ResPen => Stats[Affix.ResPen] + BonusStats[Affix.ResPen];
+    public double DazeBonus => Stats[Affix.DazeBonus] + BonusStats[Affix.DazeBonus];
+
+    public SafeDictionary<Affix, double> CollectStats() => new() {
+        [Affix.Hp] = Hp,
+        [Affix.Atk] = Atk,
+        [Affix.Def] = Def,
+        [Affix.Pen] = Pen,
+        [Affix.PenRatio] = PenRatio,
+        [Affix.CritRate] = CritRate,
+        [Affix.CritDamage] = CritDamage,
+        [Affix.Impact] = Impact,
+        [Affix.AnomalyMastery] = AnomalyMastery,
+        [Affix.AnomalyProficiency] = AnomalyProficiency,
+        [Affix.EnergyRegen] = EnergyRegen,
+        [RelatedElementDmg] = ElementalDmgBonus,
+        [RelatedElementRes] = ElementalResPen,
+        [Affix.DmgBonus] = DmgBonus,
+        [Affix.ResPen] = ResPen,
+        [Affix.DazeBonus] = DazeBonus
+    };
 
     /// <summary>
     /// Applies the agent's passive to the team
