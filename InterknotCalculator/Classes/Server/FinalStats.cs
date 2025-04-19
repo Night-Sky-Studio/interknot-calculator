9179bec0cc80138833bd39a926b4f9f6ed8f7773
LilyStilson <lunamusicoff@gmail.com>
2025-04-19 22:57:32 +0200

    feat: added FinalStats type; fix: final stats will contain both calculated and base stats

---
+++ InterknotCalculator/Classes/Server/FinalStats.cs
@@ -0,0 +1,8 @@
+﻿using InterknotCalculator.Enums;
+
+namespace InterknotCalculator.Classes.Server;
+
+public record FinalStats {
+    public Dictionary<Affix, double> BaseStats { get; set; } = new();
+    public Dictionary<Affix, double> CalculatedStats { get; set; } = new();
+}
