f9f21aa5a248d3f4473efd8a7aa4f1cbf3c01c19
LilyStilson <lunamusicoff@gmail.com>
2025-05-27 23:00:51 +0200

    fix: added Nineveh's daze value

--- InterknotCalculator/Classes/Enemies/Nineveh.cs
+++ InterknotCalculator/Classes/Enemies/Nineveh.cs
@@ -2,6 +2,7 @@ namespace InterknotCalculator.Classes.Enemies;
 
 public class Nineveh : Enemy {
     public Nineveh(double stunMultiplier = 1.5) : base(953, 794) {
+        Daze = 16747;
         StunMultiplier = stunMultiplier;
     }
 }
