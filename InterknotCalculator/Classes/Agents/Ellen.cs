581a363b154969aa35b41a73482397dcfd5bb39f
LilyStilson <lunamusicoff@gmail.com>
2025-05-30 00:38:29 +0200

    feat: added Ellen's Anomaly Buildup values

--- InterknotCalculator/Classes/Agents/Ellen.cs
+++ InterknotCalculator/Classes/Agents/Ellen.cs
@@ -22,51 +22,51 @@ public sealed class Ellen : Agent {
         Skills["saw_teeth_trimming"] = new() {
             Tag = SkillTag.BasicAtk,
             Scales = [
-                new(98.3, 37.6, Element.Physical),
-                new(222.2, 130.8, Element.Physical),
-                new(478.6, 306.8, Element.Physical),
+                new(98.3, 37.6, Element: Element.Physical),
+                new(222.2, 130.8, Element: Element.Physical),
+                new(478.6, 306.8, Element: Element.Physical),
             ]
         };
         Skills["flash_freeze_trimming"] = new() {
             Tag = SkillTag.BasicAtk,
             Scales = [
-                new(199.7, 74.1),
-                new(368.8, 135.3),
-                new(993.4, 368.7),
+                new(199.7, 74.1, 44.35),
+                new(368.8, 135.3, 81.99),
+                new(993.4, 368.7, 223.17),
             ]
         };
 
         Skills["arctic_ambush"] = new() {
             Tag = SkillTag.Dash,
             Scales = [
-                new(125, 94.2),
-                new(266.8, 152.2),
-                new(316.6, 188.9)
+                new(125, 94.2, 56.59),
+                new(266.8, 152.2, 89.19),
+                new(316.6, 188.9, 110.56)
             ]
         };
         Skills["monstrous_wave"] = new() {
             Tag = SkillTag.Dash,
             Scales = [
-                new(154, 58.3, Element.Physical)
+                new(154, 58.3, Element: Element.Physical)
             ]
         };
         Skills["cold_snap"] = new() {
             Tag = SkillTag.Dash,
             Scales = [
-                new(292, 118.4)
+                new(292, 118.4, 71.63)
             ]
         };
         Skills["reef_rock"] = new() {
             Tag = SkillTag.Counter,
             Scales = [
-                new(305.5, 341.8)
+                new(305.5, 341.8, 106.7)
             ]
         };
 
         Skills["shark_sentinel"] = new() {
             Tag = SkillTag.QuickAssist,
             Scales = [
-                new(243.2, 182.7)
+                new(243.2, 182.7, 110.03)
             ]
         };
         Skills["wavefront_impact"] = new() {
@@ -80,39 +80,39 @@ public sealed class Ellen : Agent {
         Skills["shark_cruiser"] = new() {
             Tag = SkillTag.FollowUpAssist,
             Scales = [
-                new(876.8, 577.3)
+                new(876.8, 577.3, 373.92)
             ]
         };
 
         Skills["drift"] = new() {
             Tag = SkillTag.Special,
             Scales = [
-                new(101.1, 75.8)
+                new(101.1, 75.8, 45.84)
             ]
         };
         Skills["tail_swipe"] = new() {
             Tag = SkillTag.ExSpecial,
             Scales = [
-                new(754.5, 608.6)
+                new(754.5, 608.6, 403.73)
             ]
         };
         Skills["sharknami"] = new() {
             Tag = SkillTag.ExSpecial,
             Scales = [
-                new(1106.6, 557.6)
+                new(1106.6, 557.6, 372.19)
             ]
         };
 
         Skills["avalanche"] = new() {
             Tag = SkillTag.Chain,
             Scales = [
-                new(1589.9, 533.9)
+                new(1589.9, 533.9, 522.83)
             ]
         };
         Skills["endless_winter"] = new() {
             Tag = SkillTag.Ultimate,
             Scales = [
-                new(3781.7, 278.7)
+                new(3781.7, 278.7, 168.33)
             ]
         };
     }
