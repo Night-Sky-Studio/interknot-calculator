2f90b142b4552f4e7b9a4ee05384c3f3ee685d5a
LilyStilson <lunamusicoff@gmail.com>
2025-05-30 00:38:08 +0200

    fix: adjusted Nineveh's Daze initialization

--- InterknotCalculator/Classes/Enemies/Nineveh.cs
+++ InterknotCalculator/Classes/Enemies/Nineveh.cs
@@ -2,7 +2,9 @@ namespace InterknotCalculator.Classes.Enemies;
 
 public class Nineveh : Enemy {
     public Nineveh(double stunMultiplier = 1.5) : base(953, 794) {
-        Daze = 16747;
+        Daze = new Progress {
+            Maximum = 16747
+        };
         StunMultiplier = stunMultiplier;
     }
 }
