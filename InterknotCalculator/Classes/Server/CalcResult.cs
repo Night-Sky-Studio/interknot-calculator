3d172ec7ad251111a67f05d43fa6c0b5e5f348fa
LilyStilson <lunamusicoff@gmail.com>
2025-06-05 23:34:01 +0200

    feat: added enemy data to the calc result

--- InterknotCalculator/Classes/Server/CalcResult.cs
+++ InterknotCalculator/Classes/Server/CalcResult.cs
@@ -1,3 +1,4 @@
+using InterknotCalculator.Classes.Enemies;
 using InterknotCalculator.Enums;
 
 namespace InterknotCalculator.Classes.Server;
@@ -5,5 +6,6 @@ namespace InterknotCalculator.Classes.Server;
 public class CalcResult {
     public FinalStats FinalStats { get; set; } = new();
     public IEnumerable<AgentAction> PerAction { get; set; } = [];
+    public Enemy? Enemy { get; set; }
     public double Total { get; set; }
 }
