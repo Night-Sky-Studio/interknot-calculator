c849cb3da5dc1adb5df38597930ce77ac2c7282d
LilyStilson <lunamusicoff@gmail.com>
2025-08-11 14:21:28 +0200

    refactor: removed StunAgent class, moved GetDaze function to Agent

--- InterknotCalculator/Classes/Agents/Agent.cs
+++ InterknotCalculator/Classes/Agents/Agent.cs
@@ -158,6 +158,31 @@ public abstract class Agent(uint id) {
         }];
     }
 
+    public virtual double GetDaze(string action, int scale) {
+        var data = Skills[action];
+
+        var tagDazeBonus = 1.0;
+        foreach (var stat in TagBonus) {
+            if (stat.SkillTags.Contains(data.Tag) && stat.Affix == Affix.DazeBonus) {
+                tagDazeBonus += stat.Value;
+            }
+        }
+
+        var abilityPassive = ApplyAbilityPassive(action);
+        if (abilityPassive is { Affix: Affix.DazeBonus } passive) {
+            tagDazeBonus += passive.Value;
+        }
+        
+        var dazeDmgMultiplier = data.Scales[scale].Daze;
+        var dazeIncrease = 0.0 + tagDazeBonus;
+        var dazeReduction = 0.0;
+        var dazeRes = 0.0;
+        var dazeTakenIncrease = 0.0;
+        var dazeTakenReduction = 0.0;
+        return Impact * dazeDmgMultiplier * (1 + dazeIncrease - dazeReduction)
+               * (1 - dazeRes) * (1 + dazeTakenIncrease - dazeTakenReduction);
+    }
+    
     public double GetAnomalyBuildup(string skill, int scale, SkillTag currentTag) {
         var data = Skills[skill];
         var baseBuildup = data.Scales[scale].AnomalyBuildup;
